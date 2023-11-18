2306deafefb8558292578dac7fed2278ffbbc774
Gefferson-Souza <geffersonteodorodesouza@gmail.com>
2023-11-17 23:56:46 -0300

    Title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { TitleComponent } from './components/title/title.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TitleComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
